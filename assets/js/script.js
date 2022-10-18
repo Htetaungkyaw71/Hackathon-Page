390d08caafee74712ce4d14a30cf7ec79f29c1ef
Htetaungkyaw71 <htetaung200071@gmail.com>
2022-10-19 00:21:17 +0630

    Fix background for menu and change other features

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,45 +54,61 @@ const speakers = [
     position: 'Professor at Standford University',
     description: 'Have the time to share your thought and options with expects for each topic',
   },
+  {
+    id: '5',
+    name: 'Coloe',
+    image: './assets/images/person5.jpg',
+    position: 'Professor at Newyork University',
+    description: 'Have the time to share your thought and options with expects for each topic',
+  },
+  {
+    id: '6',
+    name: 'Diana',
+    image: './assets/images/person6.jpg',
+    position: 'Professor at Landon University',
+    description: 'Have the time to share your thought and options with expects for each topic',
+  },
 
 ];
 
-speakers.forEach((speaker) => {
-  const feature = document.querySelector('.feature-body');
-  const div = document.createElement('div');
-  div.innerHTML = `
-            <div class="feature-card" id="speaker${speaker.id}">
-                    <div class="feature-img">
-                        <img src="${speaker.image}" alt="speaker-image">
-                    </div>
-             
-                    <div class="feature-content">
-                        <h1>${speaker.name}</h1>
-                        <h3>${speaker.position}</h3>
-                        <span></span>
-                        <p>
-                           ${speaker.description}
-                        </p>
-                    </div>        
-            </div>  
-    `;
-  feature.appendChild(div);
+document.addEventListener('DOMContentLoaded', () => {
+  speakers.forEach((speaker) => {
+    const feature = document.querySelector('.feature-body');
+    const div = document.createElement('div');
+    div.innerHTML = `
+              <div class="feature-card" id="speaker${speaker.id}">
+                      <div class="feature-img">
+                          <img src="${speaker.image}" alt="speaker-image">
+                      </div>
+               
+                      <div class="feature-content">
+                          <h2>${speaker.name}</h2>
+                          <h3>${speaker.position}</h3>
+                          <span></span>
+                          <p>
+                             ${speaker.description}
+                          </p>
+                      </div>        
+              </div>  
+      `;
+    feature.appendChild(div);
+  });
+
+  const more = document.querySelector('#more');
+  const speaker5 = document.getElementById('speaker5');
+  const speaker6 = document.getElementById('speaker6');
+
+  more.onclick = () => {
+    if (speaker5.style.display === '' || speaker5.style.display === 'none') {
+      more.innerHTML = 'Less <i class="fa-solid fa-chevron-up"></i>';
+      speaker5.style.display = 'flex';
+      speaker6.style.display = 'flex';
+    } else {
+      more.innerHTML = 'More <i class="fa-solid fa-chevron-down"></i>';
+      speaker5.style.display = 'none';
+      speaker6.style.display = 'none';
+    }
+  };
 });
-
-const more = document.querySelector('#more');
-const speaker3 = document.getElementById('speaker3');
-const speaker4 = document.getElementById('speaker4');
-
-more.onclick = () => {
-  if (speaker3.style.display === '' || speaker3.style.display === 'none') {
-    more.innerHTML = 'Less <i class="fa-solid fa-chevron-up"></i>';
-    speaker3.style.display = 'flex';
-    speaker4.style.display = 'flex';
-  } else {
-    more.innerHTML = 'More <i class="fa-solid fa-chevron-down"></i>';
-    speaker3.style.display = 'none';
-    speaker4.style.display = 'none';
-  }
-};
 
 // Speakers
